e0a60e6a7aa51d3d15e09b9ed128c76b49dcb442
swappea <swapneshsangle1@gmail.com>
2020-01-26 20:57:12 +0530

    Final App

---
+++ public/precache-manifest.43e3b23fd69dc09cb3e9c3efb6df19de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8a61cdb22d02e8b20cfa2a390cbe50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc5b71b37d4d620dd1e",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4419be457893ba9541eb",
+    "url": "/static/js/2.1ba82684.chunk.js"
+  },
+  {
+    "revision": "786089ef6a78275eeee7a67aba60641a",
+    "url": "/static/js/2.1ba82684.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edc5b71b37d4d620dd1e",
+    "url": "/static/js/main.29d91152.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const graphqlHttp = require('express-graphql');
 const schema = require('./schema.js');
-
+const path = require('path');
 const app = express();
+
+// Allow cors
+app.use(cors());
 
 app.use('/graphql', graphqlHttp({
     schema,
     graphiql: true
 }));
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 const PORT = process.env.PORT || 5000;
 
